f5e6d4971a9829188d3e8b8acafaa866380870f7
Cas-Akuchie Chukwuemeka Samuel <chukwuemekas818@gmail.com>
2026-03-28 10:23:24 +0100

    cleanup

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <Navbar/>
         {children}</body>
+        
     </html>
   );
 }
